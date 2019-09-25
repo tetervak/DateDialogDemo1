162b15062b1a62329eea558cb1da3e269434a0f4
Alex Tetervak <tetervak@sheridancollege.ca>
2019-09-25 14:31:53 -0400

    changed to shorter string constants

--- app/src/main/java/ca/javateacher/datedialogdemo1/Constants.java
+++ app/src/main/java/ca/javateacher/datedialogdemo1/Constants.java
@@ -1,8 +1,8 @@
 package ca.javateacher.datedialogdemo1;
 
 public class Constants {
-  static final String ABOUT_FRAGMENT_TAG = "aboutFragmentTag";
-  static final String TIME_INPUT_FRAGMENT_TAG = "timeInputFragmentTag";
-  static final String DATE_INPUT_FRAGMENT_TAG = "dateInputFragmentTag";
-  static final String DATE_KEY = "dateKey";
+  static final String ABOUT_FRAGMENT_TAG = "aboutFragment";
+  static final String TIME_INPUT_FRAGMENT_TAG = "timeInputFragment";
+  static final String DATE_INPUT_FRAGMENT_TAG = "dateInputFragment";
+  static final String DATE_KEY = "date";
 }
